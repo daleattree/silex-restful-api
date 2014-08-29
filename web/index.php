ca517ae8e5619e65a26884e4e275a11fa3b3aa89
Dale Attree <dale.attree@gmail.com>
2014-08-29 12:53:22 +0200

    Basic security
    Implemented session handling for logging in and securing certain API
    calls

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@
  * Time: 7:19 AM
  */
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
@@ -15,28 +15,45 @@ define('DEFAULT_PASSWORD', '@hackathon');
 
 $app = new Silex\Application();
 
+$app->register(new Silex\Provider\SessionServiceProvider());
+
 $app->post('/login', function (Request $request) use ($app) {
     $username = $request->get('username');
     $password = $request->get('password');
 
     $result = array('message' => 'Login successful');
     $code = 200;
-    if($username != DEFAULT_USERNAME && $password != DEFAULT_PASSWORD){
+    $app['session']->set('logged_in', true);
+    if ($username != DEFAULT_USERNAME && $password != DEFAULT_PASSWORD) {
         $result = array('message' => 'Invalid login');
-        $code = 404;
+        $code = 403;
+        $app['session']->set('logged_in', false);
     }
 
     return $app->json($result, $code);
 });
 
-$app->get('/start', function() use ($app){
-   $result = array('message' => 'Hello World!');
-   return $app->json($result, 200);
+$app->get('/logout', function () use ($app) {
+    $app['session']->set('logged_in', false);
+    $result = array('message' => 'Logged out');
+    return $app->json($result, 200);
 });
 
-$app->get('/hello/{who}', function($who) use ($app){
-   $result = array('message' => 'Hello ' . $who . '!');
-   return $app->json($result, 200);
+$app->get('/start', function () use ($app) {
+    $result = array('message' => 'Hello World!');
+    return $app->json($result, 200);
+});
+
+$app->get('/hello/{who}', function ($who) use ($app) {
+    $result = array('message' => 'Hello ' . $who . '!');
+    $code = 200;
+
+    if (!$app['session']->get('logged_in')) {
+        $result = array('message' => 'Access denied');
+        $code = 403;
+    }
+
+    return $app->json($result, $code);
 });
 
 $app->error(function (\Exception $e, $code) use ($app) {
